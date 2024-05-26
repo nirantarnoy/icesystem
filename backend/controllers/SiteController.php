08097d3b6a81e6ba73e8527bf2f6111ae472d354
Niran Wangyad <niran.w@cicnetgrp.net>
2024-05-26 22:47:08 +0700

    'update'

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -1401,7 +1401,7 @@ class SiteController extends Controller
         $findcaldate = date('Y-m-d');
         $model_cal_date = \common\models\TransactionManagerDaily::find()->max('trans_date');
         if($model_cal_date){
-           $findcaldate = date('Y-m-d H:i:s',strtotime($model_cal_date,'+1 days'));
+           $findcaldate = date('Y-m-d H:i:s',strtotime($model_cal_date,'+ 1 day'));
             $from_date = $findcaldate;
             $to_date = $findcaldate;
 
